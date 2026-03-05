7fec50721a835531a1bdd8ba4c04a8b8f5476bf8
Christoph Kappel <christoph@unexist.dev>
2026-03-05 15:17:35 +0100

    Updated paths and docheader

--- weather/src/weather.c
+++ weather/src/weather.c
@@ -1,3 +1,14 @@
+//
+// @package sublets-rs
+//
+// @file Weather plugin functions
+// @copyright (c) 2025-present Christoph Kappel <christoph@unexist.dev>
+// @version $Id$
+//
+// This program can be distributed under the terms of the GNU GPLv3.
+// See the file LICENSE for details.
+//
+
 #define EXTISM_IMPLEMENTATION
 #include "extism-pdk.h"
 #include <stdint.h>
