19b16c7ee81103507a5eacbc6be667fcde840f1e
Christoph Kappel <christoph@unexist.dev>
2026-03-09 16:34:46 +0100

    Updated config and error handling

--- weather/src/weather.c
+++ weather/src/weather.c
@@ -2,7 +2,7 @@
 // @package sublets-rs
 //
 // @file Weather plugin functions
-// @copyright (c) 2025-present Christoph Kappel <christoph@unexist.dev>
+// @copyright (c) 2026-present Christoph Kappel <christoph@unexist.dev>
 // @version $Id$
 //
 // This program can be distributed under the terms of the GNU GPLv3.
@@ -10,16 +10,54 @@
 //
 
 #define EXTISM_IMPLEMENTATION
+
 #include "extism-pdk.h"
+
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+char *get_config_key(const char *key, const char *fallback) {
+    ExtismHandle value = extism_config_get(extism_alloc_buf_from_sz(key));
+
+    if (0 == value) {
+        value = extism_alloc_buf_from_sz(fallback);
+    }
+
+    const uint64_t len = extism_length(value);
+    char *data = (char *)extism_malloc(value);
+
+    if (NULL == data) {
+        ExtismHandle err = extism_alloc_buf_from_sz("OOM");
+
+        extism_error_set(err);
+
+        return NULL;
+    }
+
+    extism_log_debug(value);
+
+    extism_load_from_handle(value, 0, data, len);
+
+    return data;
+}
 
 int32_t EXTISM_EXPORTED_FUNCTION(run) {
-    const char *reqStr = "{\
-        \"method\": \"GET\",\
-        \"url\": \"https://api.open-meteo.com/v1/forecast?latitude=51.4566&longitude=7.0123&daily=temperature_2m_max&forecast_days=1\"\
-    }";
+    char *latitude = get_config_key("latitude", "51.4566");
+    char *longitude = get_config_key("longitude", "7.0123");
 
-    ExtismHandle req = extism_alloc_buf_from_sz(reqStr);
+    if (NULL == latitude || NULL == longitude) {
+        return -1;
+    }
+
+    char buf[255] = { 0 };
+
+    sprintf(buf, "{\
+        \"method\": \"GET\",\
+        \"url\": \"https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=temperature_2m_max&forecast_days=1\"\
+    }", latitude, longitude);
+
+    ExtismHandle req = extism_alloc_buf_from_sz(&buf);
     ExtismHandle res = extism_http_request(req, 0);
 
     if (200 != extism_http_status_code()) {
@@ -27,6 +65,9 @@ int32_t EXTISM_EXPORTED_FUNCTION(run) {
     }
 
     extism_output_set_from_handle(res, 0, extism_length(res));
+
+    extism_free(latitude);
+    extism_free(longitude);
 
     return 0;
 }
