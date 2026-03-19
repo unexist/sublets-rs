9897702e721c94578dcd1e9ae385d6dee83a5599
Christoph Kappel <christoph@unexist.dev>
2026-03-19 15:18:40 +0100

    Added plugin stub

--- cpu/src/Main.hs
+++ cpu/src/Main.hs
@@ -1,4 +1,22 @@
-module Main where
+{-# LANGUAGE DeriveDataTypeable #-}
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+module Hello where
+
+import Data.Maybe
+import Extism.PDK
+import Extism.PDK.JSON
+
+defaultGreeting = "Hello"
+
+greet g n =
+  output $ g ++ ", " ++ n
+
+testing = do
+  -- Get a name from the Extism runtime
+  name <- inputString
+  -- Get  configured greeting
+  greeting <- getConfig "greeting"
+  -- Greet the user, if no greeting is configured then "Hello" is used
+  greet (fromMaybe defaultGreeting greeting) name
+
+foreign export ccall "greet" testing :: IO ()
