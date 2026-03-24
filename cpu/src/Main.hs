7623670cdc7de1c41e28b19e226512a15bb761bf
Christoph Kappel <christoph@unexist.dev>
2026-03-24 12:29:30 +0100

    Added doc header

--- cpu/src/Main.hs
+++ cpu/src/Main.hs
@@ -1,3 +1,14 @@
+--
+-- @package sublets-rs
+--
+-- @file CPU plugin functions
+-- @copyright (c) 2026-present Christoph Kappel <christoph@unexist.dev>
+-- @version $Id$
+--
+-- This program can be distributed under the terms of the GNU GPLv3.
+-- See the file LICENSE for details.
+--
+
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Hello where
